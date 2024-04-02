23b695f14d76d8e735f35349c14e4fdfaa0c75d9
Ankita <Ankitashejal11@gmail.com>
2024-04-02 12:53:25 +0530

    profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,12 @@ import { personCircleOutline, homeOutline, paperPlaneSharp, heartOutline, heartS
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Profile', url: '/folder/Profile', icon: 'person-circle-outline' },
+    { title: 'Profile', url: '/profile', icon: 'person-circle-outline' },
     { title: 'Home', url: '/folder/Home', icon: 'home-outline' },
-    // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    // { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    // { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    // { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {
